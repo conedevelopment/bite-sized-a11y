2664d10d0829e2e25cbeab2b127ce06616bc9d45
Adam Laki <info@adamlaki.com>
2022-07-09 18:11:52 +0200

    WIP

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
 module.exports = config => {
   return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
     dir: {
       input: 'src',
       output: 'dist'
     }
   };
 };
+
+// Set directories to pass through to the dist folder
+config.addPassthroughCopy('./src/img/');
---
+++ src/index.md
@@ -0,0 +1,9 @@
+---
+title: "Hello, world"
+layout: "layouts/home.html"
+hero:
+  title: "Bite-Sized Accessibility"
+  description: "Quick and small (less than 250 words) accessibility tips for the one who's in a rush or just one to learn something new."
+---
+
+Hello home
