6b55ce043c20f1a1fe38d533f61294127990b7bd
Adam Laki <info@adamlaki.com>
2022-12-29 11:18:33 +0100

    Update date format

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -2,5 +2,5 @@ const moment = require('moment');
 
 module.exports = value => {
   const dateObject = moment(value);
-  return `${dateObject.format('Do')} of ${dateObject.format('MMMM YYYY')}`;
+  return dateObject.format('Do MMMM YYYY');
 };
